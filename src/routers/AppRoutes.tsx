52374690759ef0b940484bad270deadae5372b05
Brittney <brittney.cavazos@gmail.com>
2021-05-11 14:59:10 -0700

    built basic skeleton of App Router

---
+++ src/routers/AppRoutes.tsx
@@ -0,0 +1,22 @@
+import { Switch, Route } from 'react-router-dom';
+// page components
+/* import Homepage from '../pages/Homepage';
+import Singlepage from '../pages/Singlepage'; */
+// Components
+/* import Navbar from '../components/Navbar' */
+
+const AppRoutes = () =>{
+    return (
+        <div>
+          {/* <Navbar />
+          <div className='container-fluid'>
+            <Switch>
+              <Route path='/' exact component={Homepage} />
+              <Route path='/cocktails/:cocktailId' component={Singlepage} />
+            </Switch>
+          </div> */}
+        </div>
+      );
+}
+
+export default AppRoutes
