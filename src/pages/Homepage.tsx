04284cd5cbad6ef71c0734b6b50a15c761177522
Brittney <brittney.cavazos@gmail.com>
2021-05-11 14:40:22 -0700

    feat(homepage)skeleton built

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Homepage = () =>{
+    <div className="row">
+        <div className="col-md-8 col-offset-2">
+            <p>homepage test test</p>
+        </div>
+    </div>
+}
+
+export default Homepage
