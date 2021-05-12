d81c033f9f5065c1b17f3b37067ce6ccec861b86
Brittney <brittney.cavazos@gmail.com>
2021-05-12 09:49:38 -0700

    updating code so that it can render in browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router} from 'react-router-dom';
+import AppRoutes from './routers/AppRoutes';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Homepage
-      </header>
+      <Router>
+        <AppRoutes/>
+      </Router>
     </div>
   );
 }
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const Homepage = () =>{
-    <div className="row">
-        <div className="col-md-8 col-offset-2">
-            <p>homepage test test</p>
-        </div>
-    </div>
+    return(
+        <div className="row">
+            <div className="col-md-8 col-offset-2">
+                <p>homepage test test</p>
+            </div>
+        </div>        
+    )
 }
 
 export default Homepage
